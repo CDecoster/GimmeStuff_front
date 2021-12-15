30b137940ed29d7a617601f9aebfd2703bbfe8ed
Axel De Clerck <axel.declerck@hotmail.com>
2021-12-15 13:59:48 +0100

    Add navbar
    Add navbar component + folder

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 // Custom styles
 import './stylesheets/main.css';
+import Navbar from "./Components/Navbar/Navbar";
 
-// This is the entry point to your app : add all relevant import and custom code
+// Navbar layout
+Navbar();
 
