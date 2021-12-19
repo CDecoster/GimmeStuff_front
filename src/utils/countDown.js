ecf3dc29e800104fee32e23b35b981a568eeaca0
nicolasnederlandt <74932969+nicolasnederlandt@users.noreply.github.com>
2021-12-19 19:11:53 +0100

    commit
    commit

--- src/utils/countDown.js
+++ src/utils/countDown.js
@@ -12,10 +12,15 @@ function countDown(dateString, usage, element) {
 
         // Find the distance between now and the count down date
         var distance = countDownDate - now;
-
+        
+        while(distance<0){
+            distance = distance+365*1000 * 60 * 60 * 24;
+        } 
+        
 
         // Time calculations for days, hours, minutes and seconds
         var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+       
         var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
         var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
         var seconds = Math.floor((distance % (1000 * 60)) / 1000);
